93a94854b7d443522df68894ecfee2b94c93f475
nub8p <sujin4418618@gmail.com>
2023-05-13 08:09:39 +0900

    Recipe Page layout complete

--- lib/Screen/RecipeScreen.dart
+++ lib/Screen/RecipeScreen.dart
@@ -5,85 +5,148 @@ class RecipeScreen extends StatelessWidget {
 
   RecipeScreen({required this.selectedItems});
 
-  Widget _buildSelectedItems() {
+  Widget _buildSelectedItems(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(16.0),
       decoration: BoxDecoration(
-        color: Colors.grey[200],
-        borderRadius: BorderRadius.circular(10.0),
+        color: Colors.black12,
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Selected Ingredients:',
-            style: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.black87,
-            ),
-          ),
-          SizedBox(height: 8.0),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: selectedItems.map((item) {
-              return Padding(
-                padding: EdgeInsets.symmetric(vertical: 4.0),
-                child: Text(
-                  item,
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    color: Colors.black54,
-                  ),
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SingleChildScrollView(
+                child: Wrap(
+                  spacing: 12.0,
+                  runSpacing: 12.0,
+                  children: selectedItems.map((item) {
+                    return AspectRatio(
+                      aspectRatio: 1.0,
+                      child: Container(
+                        padding: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          color: Colors.grey[700],
+                          borderRadius: BorderRadius.circular(6.0),
+                        ),
+                        child: Center(
+                          child: Text(
+                            item,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    );
+                  }).toList(),
                 ),
-              );
-            }).toList(),
+              ),
+            ),
           ),
         ],
       ),
     );
   }
 
-  @override
+  Widget _buildRecipeCard(String title, String description) {
+    return Container(
+      width: 200.0,
+      height: 200,
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                description,
+                style: TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.grey[600],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRecipeList(String title) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 16.0),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: <Widget>[
+              SizedBox(width: 14.0),
+              _buildRecipeCard('Recipe 1', 'Description of Recipe 1'),
+              SizedBox(width: 14.0),
+              _buildRecipeCard('Recipe 2', 'Description of Recipe 2'),
+              SizedBox(width: 14.0),
+              _buildRecipeCard('Recipe 3', 'Description of Recipe 3'),
+              SizedBox(width: 14.0),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Recipes'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            _buildSelectedItems(),
-            SizedBox(height: 16.0),
-            Text(
-              'Recommended Recipes:',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.black87,
+      body: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: _buildSelectedItems(context),
+          ),
+          Expanded(
+            flex: 5,
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("집술이수리 마수리",
+                      style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w900,),
+                    ),
+                    SizedBox(height: 20.0),
+                    _buildRecipeList('만들 수 있어요.'),
+                    SizedBox(height: 40.0),
+                    _buildRecipeList('하나가 부족해요.'),
+                    SizedBox(height: 40.0),
+                    _buildRecipeList('가서 사와 이놈아.'),
+                  ],
+                ),
               ),
             ),
-            SizedBox(height: 16.0),
-            Expanded(
-              child: ListView.builder(
-                itemCount: 5, // 여기에 실제 레시피 리스트의 개수를 입력하세요.
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    leading: Icon(Icons.local_dining),
-                    title: Text('Recipe ${index + 1}'),
-                    subtitle: Text('Description of Recipe ${index + 1}'),
-                    onTap: () {
-                      // 레시피 상세 페이지로 이동
-                    },
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
+
 }
