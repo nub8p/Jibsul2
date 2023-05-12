00d99b64739984ad3cf16c1ca66bdcd1290cccda
eunbin777 <103042314+eunbin777@users.noreply.github.com>
2023-05-13 08:06:33 +0900

    setting firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,16 @@ import 'package:sogra/Screen/FlatterScreen.dart';
 import 'package:sogra/Screen/HomeScreen.dart';
 import 'package:sogra/Screen/MyScreen.dart';
 import 'package:sogra/Screen/SelectScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -22,13 +29,11 @@ class myApp extends StatefulWidget {
   const myApp({super.key});
 
   @override
-  State<myApp> createState() =>
-      _myApp();
+  State<myApp> createState() => _myApp();
 }
 
 // 스크린 이동
-class _myApp
-    extends State<myApp> {
+class _myApp extends State<myApp> {
   int _selectedIndex = 0;
   static List<Widget> _widgetOptions = <Widget>[
     HomeScreen(),
